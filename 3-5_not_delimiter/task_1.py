236fa29a4eb13e21252da224315e54918d61cd99
roman.lukyanov <roman.lukyanov@x5.ru>
2022-03-28 23:25:23 +0300

    Default commit message

---
+++ 3-5_not_delimiter/task_1.py
@@ -0,0 +1,6 @@
+print('Задача 1. Яблоки')
+apple_weight = 41
+box_capasity = 3
+
+print(apple_weight, 'тонн яблок можно пометить в', apple_weight // box_capasity, 'коробок')
+print(apple_weight % box_capasity, 'тонн яблок останется')
---
+++ 3-5_not_delimiter/task_2.py
@@ -0,0 +1,4 @@
+print('Задача 2. Последняя цифра')
+home_street_number = int(input('Введите номер квартиры и дома без пробелов: '))
+print('Номер дома', home_street_number % 10)
+print('Номер квартиры', home_street_number // 10)
